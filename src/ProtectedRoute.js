c8830081e293d6069d2b89a8c79adbbb18e2d57f
Fergus Yip <fergus.yipwailam@gmail.com>
2020-09-02 18:32:07 +1000

    Add loading animation

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
+import { Result, Spin } from 'antd';
 
 const ProtectedRoute = ({ component: Component, auth, ...rest }) => {
-  return (
-    auth.isLoaded &&
-    (auth.uid ? (
+  return auth.isLoaded ? (
+    auth.uid ? (
       <Route {...rest} render={(props) => <Component {...rest} {...props} />} />
     ) : (
       <Redirect to="/login" />
-    ))
+    )
+  ) : (
+    <Result icon={<Spin size="large" />} title="Loading" />
   );
 };
 
