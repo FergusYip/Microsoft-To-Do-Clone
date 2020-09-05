84218889275fb54c92347d0252eec334b485c318
Fergus Yip <fergus.yipwailam@gmail.com>
2020-09-05 14:00:24 +1000

    Handle step rename

--- src/components/TodoMenu/StepItem.js
+++ src/components/TodoMenu/StepItem.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
-import { List, Checkbox, Button } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { List, Checkbox, Button, Input } from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
 
 export default function StepItem({ step, onUpdate, onRemove }) {
+  const [editingTitle, setEditingTitle] = useState(false);
+
   function handleOnChange(e) {
     onUpdate({ ...step, isComplete: e.target.checked });
   }
@@ -11,19 +13,58 @@ export default function StepItem({ step, onUpdate, onRemove }) {
     onRemove(step);
   }
 
+  function handleRename(e) {
+    e.preventDefault();
+    document.activeElement.blur();
+    setEditingTitle(false);
+    const newTitle = e.target.value.trim();
+    if (newTitle && newTitle !== step.title) {
+      onUpdate({ ...step, title: newTitle });
+    }
+  }
+
+  const handleTitleFocus = () => setEditingTitle(true);
+
   return (
     <List.Item
-      actions={[
-        <Button type="text" shape="circle" onClick={handleRemove}>
-          <CloseOutlined style={{ fontSize: 12 }} />
-        </Button>,
-      ]}
+      actions={
+        editingTitle
+          ? []
+          : [
+              <Button
+                type="text"
+                shape="circle"
+                onClick={handleRemove}
+                style={{ padding: 0, height: 26, width: 26 }}
+              >
+                <CloseOutlined />
+              </Button>,
+            ]
+      }
     >
       <List.Item.Meta
         avatar={
           <Checkbox checked={step.isComplete} onChange={handleOnChange} />
         }
-        title={step.title}
+        title={
+          <Input.TextArea
+            className="ant-typography"
+            bordered={false}
+            defaultValue={step.title}
+            style={{
+              padding: 0,
+              color: 'rgba(0, 0, 0, 0.85)',
+              fontSize: 14,
+              lineHeight: '1.5715',
+              resize: 'none',
+            }}
+            autoSize={{ minRows: 1 }}
+            maxLength={100}
+            onPressEnter={handleRename}
+            onBlur={handleRename}
+            onFocus={handleTitleFocus}
+          />
+        }
       ></List.Item.Meta>
     </List.Item>
   );
